5eddd7d8faf114c108b40ef4e117682a13d70752
TheTobiTiger02 <Tobias.Heyn@stud.h-da.de>
2025-11-23 21:21:35 +0100

    fixed build

--- app/src/main/java/com/example/pokemonalertsv2/ui/theme/Color.kt
+++ app/src/main/java/com/example/pokemonalertsv2/ui/theme/Color.kt
@@ -47,3 +47,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Typography colors
+val PokeWhite = Color(0xFFFFFFFF)
+val PokeLightGray = Color(0xFFE3E8FF) // Matching onBackground/onSurface somewhat
+val PokeDarkGray = Color(0xFFB8C0FF)  // Matching onSurfaceVariant somewhat
