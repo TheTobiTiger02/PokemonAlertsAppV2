ac134fc98508cd6b7bb1d561520e3877f559fe1f
TheTobiTiger02 <Tobias.Heyn02@gmail.com>
2026-07-10 17:25:31 +0200

    chore: bump version to 1.0.1 for automated release test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,8 +58,8 @@ android {
         applicationId = "com.example.pokemonalertsv2"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resValue("string", "maps_api_key", googleMapsApiKey)
